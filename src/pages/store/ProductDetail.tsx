fba2c3da4631e78ea860253f4323bba61093016c
younes1233 <younesossman12@gmail.com>
2025-06-27 20:34:00 +0300

    pagination fixed

--- src/pages/store/ProductDetail.tsx
+++ src/pages/store/ProductDetail.tsx
@@ -267,7 +267,7 @@ const ProductDetail = () => {
                           }}
                           className={`w-1.5 h-1.5 rounded-full transition-all duration-300 ${
                             index === selectedImage
-                              ? 'bg-white scale-125'
+                              ? 'bg-black scale-125'
                               : 'bg-white/50 hover:bg-white/75'
                           }`}
                         />
