6a6f7f42438c78437cea1e7bd2e317a88c4dd2bb
younes1233 <younesossman12@gmail.com>
2025-09-18 13:56:37 +0300

    country fixed

--- src/components/store/StoreLayout.tsx
+++ src/components/store/StoreLayout.tsx
@@ -177,7 +177,7 @@ const goAll = () => {
                 Categories
                 <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
               </Link>
-              <Link to="/store/returns" className="text-gray-700 hover:text-blue-600 transition-all duration-300 font-medium relative group text-sm xl:text-base">
+              {/* <Link to="/store/returns" className="text-gray-700 hover:text-blue-600 transition-all duration-300 font-medium relative group text-sm xl:text-base">
                 Returns
                 <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
               </Link>
@@ -188,7 +188,7 @@ const goAll = () => {
               <button className="text-gray-700 hover:text-blue-600 transition-all duration-300 font-medium relative group text-sm xl:text-base">
                 About
                 <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
-              </button>
+              </button> */}
             </nav>
 
             {/* Search Bar - Desktop */}
